5d39936a63c55faba8241bd8f3c5f89800f01d2b
Aichi Chang <phoebexxxc@gmail.com>
2020-08-11 23:12:50 +0100

    ready for deploy

--- backend/controllers/term.js
+++ backend/controllers/term.js
@@ -1,6 +1,7 @@
 const Terms = require('../models/Terms')
 
 function create(req, res) {
+  req.body.user = req.curUser
   Terms
     .create(req.body)
     .then(termCreated => res.status(201).json({ message: 'One Term Created', termCreated }))
@@ -27,21 +28,35 @@ function readOne(req, res) {
 function update(req, res) {
   Terms
     .findById(req.params.id)
-    .then(ters => {
-      if (!ters) return res.status(404).json({ message: 'Term Not Found' })
-      return ters.set(req.body)
+    .then(async (ters) => {
+      if(!ters) return res.status(404).json({ message: 'Term Not Found' })
+
+      if(ters.author.email !== req.curUser.email && req.curUser.role !== 'admin' && req.curUser.role !== 'mentor') {
+        return res.status(403).json({ message: 'You Have No Access To Update This Term' })
+      } else {
+        await ters.set(req.body)
+        await ters.save()
+      }
+
+      res.status(202).json(ters)
     })
-    .then(ters => ters.save())
-    .then(ters => res.status(202).json(ters))
-    .catch(errr => res.json(err))
+    // .then(ters => ters.save())
+    // .then(ters => res.status(202).json(ters))
+    .catch(err => res.json(err))
 }
 
 
 function remove(req, res) {
+
   Terms
     .findById(req.params.id)
-    .then(ters => {
+    .then((ters) => {
       if (!ters) return res.status(404).json({ message: 'Term Not Found' })
+
+      if(ters.author.email !== req.curUser.email && req.curUser.role !== 'admin' && req.curUser.role !== 'mentor') {
+        return res.status(403).json({ message: 'You Have No Access To Delete This Term' })
+      }
+
       return ters.remove()
     })
     .then(() => res.status(200).json({ message: 'Term Deleted' }))
