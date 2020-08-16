bdadc56cdce86a4e1ab125bf30d0f594e664e041
Bex B <bexb1@hotmail.com>
2020-08-16 12:50:46 +0100

    Id key added to Mongoose schema.

--- backend/models/Terms.js
+++ backend/models/Terms.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
-const Users = require('./Users')
+const mongoose = require("mongoose");
+const Users = require("./Users");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const termsSchema = new Schema({
+  id: { type: Number },
   name: { type: String },
   discription: { type: String },
   link: { type: String },
-  tags: [{ type: String }]
-})
+  tags: [{ type: String }],
+});
 
-
-module.exports = mongoose.model('Terms', termsSchema)
+module.exports = mongoose.model("Terms", termsSchema);
