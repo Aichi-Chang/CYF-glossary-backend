b1f1d3dd14ec4101826d11e8521c967250bf70f8
Aichi Chang <phoebexxxc@gmail.com>
2020-08-16 21:58:06 +0100

    add unique to id and name in Terms.js

--- backend/models/Terms.js
+++ backend/models/Terms.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 const Users = require("./Users");
+const uniqueValidator = require('mongoose-unique-validator')
+
 
 const Schema = mongoose.Schema;
 
 const termsSchema = new Schema({
-  id: { type: Number },
-  name: { type: String },
+  id: { type: Number, unique: true, required: true },
+  name: { type: String, required: true, unique: true },
   discription: { type: String },
   link: { type: String },
   tags: [{ type: String }],
 });
+
+userSchema.plugin(uniqueValidator, { message: 'Error, expected {PATH} to be unique.' })
 
 module.exports = mongoose.model("Terms", termsSchema);
